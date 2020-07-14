9eca32c514de29e11bb7afa72a51401a0ef42811
Alexandre Feio <alefeio@gmail.com>
2020-07-14 05:43:07 -0300

    up2

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,12 @@
 
 module.exports = {
   dialect: 'postgres',
-  host: '192.168.99.100',
-  // host: 'localhost',
+  // host: '192.168.99.100',
+  host: 'localhost',
   username: 'postgres',
   password: '301159',
-  // database: 'methodusldm2',
-  database: 'methodusldm',
+  database: 'methodusldm2',
+  // database: 'methodusldm',
   // host: process.env.BD_HOST,
   // username: process.env.DB_USER,
   // password: process.env.DB_PASS,
