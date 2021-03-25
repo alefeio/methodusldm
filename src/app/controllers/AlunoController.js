9ca3c8f828b8ba0a9b146d1febcffd8dd4963452
Alexandre Feio <alefeio@gmail.com>
2021-03-25 13:18:21 -0300

    alteracao listagem alunos

--- src/app/controllers/AlunoController.js
+++ src/app/controllers/AlunoController.js
@@ -4,6 +4,7 @@ class AlunoController {
   async index(req, res) {
     const usuarios = await Usuario.findAll({
       where: { admin: false },
+      order: [['updated_at', 'DESC']],
       attributes: ['id', 'nome', 'email', 'cpf'],
     });
 
